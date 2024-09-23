4dd525f2a5607c5c08ab76d6d1e79181476e0693
RoyAngelAnconeyra <rchoquehuancaan@unsa.edu.pe>
2024-09-22 23:08:05 -0500

    add Multiplicación por bloques

--- lab1.cpp
+++ lab1.cpp
@@ -33,6 +33,23 @@ Tiempo:1222 microsegundos
 // 50
 Tiempo:4741 microsegundos
 
+3. Ejercicio
+// 50 - 5 block
+Tiempo: 6908 microsegundos
+// 50 - 10 block
+Tiempo: 6524 microsegundos
+// 50 - 25 block
+Tiempo: 6790 microsegundos
+// 100 - 10 block
+Tiempo: 57 001 microsegundos
+// 100 - 20 block
+Tiempo: 51 125 microsegundos
+// 100 - 25 block
+Tiempo: 45 255 microsegundos
+// 1000 - 20 block
+Tiempo: 48 350 158 microsegundos
+// 1000 - 200 block
+Tiempo: 45 969 910 microsegundos
 
 */
 
@@ -41,9 +58,10 @@ Tiempo:4741 microsegundos
 #include <random>
 using namespace std;
 
-const int MAX = 50;
+const int MAX = 1000;
 
 double A[MAX][MAX], x[MAX], y[MAX];
+int blockSize = 200;
 
 void primer_ejercicio(){
     cout<<"Tamaño: "<<MAX<<endl;
@@ -91,11 +109,16 @@ void primer_ejercicio(){
     cout << "Segundo bucle: " << duration.count() << " microsegundos" << endl;
 }
 
-void segundo_ejercicio(){
+void segundo_tercer_ejercicio(){
     vector<vector<int>> A (MAX,vector<int>(MAX, 0));
     vector<vector<int>> B (MAX,vector<int>(MAX, 0));
-    vector<vector<int>> C(MAX, vector<int>(MAX, 0));
+    vector<vector<int>> C (MAX, vector<int>(MAX, 0));
 
+    if (MAX % blockSize != 0){
+        cout<<"Error: 'MAX' debe ser divisible por 'blockSize'"<<endl;
+        return;
+    }
+    /*
     //Llenado aleatorio
     for (int i = 0; i < MAX; ++i) {
         for (int j = 0; j < MAX; ++j) {
@@ -108,10 +131,12 @@ void segundo_ejercicio(){
             B[i][j] = rand() % 100;  
         }
     }
+    */
 
     //Tiempo
     auto start_time = chrono::high_resolution_clock::now();
-    //Multiplicación
+    //Multiplicación clásica
+    /*
     for (int i = 0; i < MAX; ++i) {
         for (int j = 0; j < MAX; ++j) {
             for (int k = 0; k < MAX; ++k) {
@@ -119,10 +144,31 @@ void segundo_ejercicio(){
             }
         }
     }
-
+    */
     auto end_time = chrono::high_resolution_clock::now();
     auto duration = chrono::duration_cast<chrono::microseconds>(end_time - start_time);
-    cout << "Tiempo - "<<MAX<<": " << duration.count() << " microsegundos" << endl;
+    cout << "Tiempo multipliacion clasica- "<<MAX<<": " << duration.count() << " microsegundos" << endl;
+
+    //Multiplicación por bloques
+    start_time = chrono::high_resolution_clock::now();
+    for(int i0 = 0; i0 < MAX; i0 += blockSize){
+        for(int j0 = 0; j0 < MAX; j0 += blockSize){
+            for(int k0 = 0; k0 < MAX; k0 += blockSize){
+                //Multiplicación
+                for(int i = i0; i < min(i0 + blockSize, MAX); ++i){
+                    for(int j = j0; j < min(j0 + blockSize, MAX); ++j){
+                        for(int k = k0; k < min(k0 + blockSize, MAX); ++k){
+                            C[i][j] += A[i][k] * B[k][j];
+                        }
+                    }
+                }
+            }
+        }
+    }
+    end_time = chrono::high_resolution_clock::now();
+    duration = chrono::duration_cast<chrono::microseconds>(end_time - start_time);
+    cout << "Tiempo multiplicacion por bloques "<<blockSize<<" - "<<MAX<<": " << duration.count() << " microsegundos" << endl;
+
     /*
     // Mostramos el resultado
     cout << "Resultado de la multiplicación de matrices:" << std::endl;
@@ -139,7 +185,7 @@ void segundo_ejercicio(){
 
 int main() {
     //primer_ejercicio();
-    segundo_ejercicio();
+    segundo_tercer_ejercicio();
 
     return 0;
 }
